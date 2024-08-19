05826547c64c26c722850736f26c807a340ac84b
dokdom <127286412+dokdom@users.noreply.github.com>
2024-08-19 17:01:03 +0900

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+  alias = "primary_region"
+}
